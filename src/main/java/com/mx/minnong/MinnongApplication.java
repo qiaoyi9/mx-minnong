eda7d8f5c03d0d55b92c575e24aef71a456ea6c9
joe <1815066657@qq.com>
2018-11-19 16:03:19 +0800

    增加redis

--- src/main/java/com/mx/minnong/MinnongApplication.java
+++ src/main/java/com/mx/minnong/MinnongApplication.java
@@ -2,10 +2,12 @@ package com.mx.minnong;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cache.annotation.EnableCaching;
 import tk.mybatis.spring.annotation.MapperScan;
 
 @SpringBootApplication
 @MapperScan(basePackages = "com.mx.minnong.mapper")
+@EnableCaching
 public class MinnongApplication {
 
 	public static void main(String[] args) {
