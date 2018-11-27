1819fffcf6fe39825b100726df18ff5dfda34099
joe <1815066657@qq.com>
2018-11-27 22:14:30 +0800

    合并JoeDev_Branch分支 1.添加serverpath：/minnongApi 2.添加学术论文模块Api /Paper 3.添加PageVo视图对象，用于接收前台访问分页Api分页数据

--- src/main/java/com/mx/minnong/config/WebContextFilter.java
+++ src/main/java/com/mx/minnong/config/WebContextFilter.java
@@ -22,7 +22,7 @@ public class WebContextFilter implements Filter {
         HttpServletRequest request=(HttpServletRequest)servletRequest;
         res.setContentType("textml;charset=UTF-8");
         res.setHeader("Access-Control-Allow-Origin", request.getHeader("Origin"));
-        res.setHeader("Access-Control-Allow-Methods", "POST, GET, OPTIONS, DELETE");
+        res.setHeader("Access-Control-Allow-Methods", "POST, GET, OPTIONS, DELETE ,PUT");
         res.setHeader("Access-Control-Max-Age", "0");
         res.setHeader("Access-Control-Allow-Headers", "Origin, No-Cache, X-Requested-With, If-Modified-Since, Pragma, Last-Modified, Cache-Control, Expires, Content-Type, X-E4M-With,userId,token");
         res.setHeader("Access-Control-Allow-Credentials", "true");
